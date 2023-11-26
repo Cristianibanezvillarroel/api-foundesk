9d2988d173c3eefebfb814ab6eba82425ea07f19
unknown <cristian.ibanez@facturaenlinea.cl>
2023-11-26 08:31:28 -0300

    agregamos corchetes para array

--- src/controllers/Blog.controller.js
+++ src/controllers/Blog.controller.js
@@ -3,16 +3,19 @@ const Blog = require('../models/Blog.model')
 const getBlog = async (req, res) => {
     try {
         const resp = await Blog.find()
-        return res.json({
+        return res.json([{
             message: 'Blog',
             items: resp
-        })
+        }])
+
+        const dataBlogs = readDataBlogs()
+        res.json(dataBlogs.Blogs)
     } catch (error) {
         return res.json({
             messaje: 'Error',
             detail: error.message
         })
-        
+
     }
 }
 
