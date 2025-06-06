771016816e504bdf025df56ac6402a9ab7c59b6f
unknown <cristian.ibanez@facturaenlinea.cl>
2025-06-06 18:57:08 -0400

    inhabilite el reader adicional a cors

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const corsOptions = {
     optionsSuccessStatus: 204
 }
 
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     //res.header("'Access-Control-Allow-Origin' : 'https://cristianibanezvillarroel.github.io'");
     res.header("'Access-Control-Allow-Origin' : '*'");
     res.header("'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'");
     next();
-})
+})*/
 
 app.use(cors(corsOptions))
 
