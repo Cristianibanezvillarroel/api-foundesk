37c99e8ea1f97ee1a00357dd5ffb857c64b17725
unknown <cristian.ibanez@facturaenlinea.cl>
2025-11-22 01:40:07 -0300

    agrega secret a ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,8 @@ module.exports = {
         MONGO_URI: "mongodb+srv://mongo:mongo@cluster0.jfxgglv.mongodb.net/foundesk?retryWrites=true&w=majority",
         MAIL_USER: "cristian.ibanez@foundesk.cl",
         MAIL_PASS: "caiv2409%C",
-        FRONTEND_URL: "https://sistema.foundesk.cl"
+        FRONTEND_URL: "https://sistema.foundesk.cl",
+        SECRET_JWT: "opciones"
       }
     }
   ]
