2a39c5dceeca30ebfa6949613626327f29ceae12
unknown <cristian.ibanez@facturaenlinea.cl>
2025-11-27 00:50:05 -0300

    modificacion a mongo uri para conexion en VM

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,10 @@ module.exports = {
       name: "api-foundesk",
       script: "index.js",
       env: {
+        NODE_ENV: "production",
         PORT: 3000,
-        MONGO_URI: "mongodb+srv://mongo:mongo@cluster0.jfxgglv.mongodb.net/foundesk?retryWrites=true&w=majority",
+        //MONGO_URI: "mongodb+srv://mongo:mongo@cluster0.jfxgglv.mongodb.net/foundesk?retryWrites=true&w=majority",
+        MONGO_URI: "mongodb://foundeskApp:oshaek3Ooth1odu@192.168.3.103:27017/foundesk?authSource=foundesk",
         MAIL_USER: "cristian.ibanez@foundesk.cl",
         MAIL_PASS: "caiv2409%C",
         FRONTEND_URL: "https://sistema.foundesk.cl",
